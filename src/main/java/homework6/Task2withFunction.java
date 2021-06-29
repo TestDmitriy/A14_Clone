ac33e74b68aa053f170e8fd55c2b875a987b09c6
voronovski <avoronovski@gmail.com>
2021-06-28 20:53:00 -0500

    Create function sort - which takes  int[] as a param and print out sorted array invoke it from main() - pass {3,9,1,8,4,8,5} as a param (this will be void func)

--- src/main/java/homework6/Task2withFunction.java
+++ src/main/java/homework6/Task2withFunction.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Task2withFunction {
     public static void main(String[] args) {
-        int[] numbers = {3,9,1,8,4,8,5};
+        int[] numbers = {3,9,1,8,4,8,5,6};
         for (int n : sort(numbers)) {
             System.out.print(n+", ");
         }
