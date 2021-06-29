74e5250ad50e4255dce08aa476b9d489fcf2bfae
voronovski <avoronovski@gmail.com>
2021-06-29 11:20:19 -0500

    Create 2 Enums. Create 2 Variables of each enum

--- src/main/java/homework7/Task1.java
+++ src/main/java/homework7/Task1.java
@@ -5,12 +5,16 @@ package homework7;
 
 public class Task1 {
     public static void main(String[] args) {
+
+//        ATMs numbers
         ATMs atmRussia = ATMs.BRU08099;
         ATMs atmHongKong = ATMs.BHK07202;
 
+//        Computers processers
         Processors comp1 = Processors.AMD;
         Processors comp2 = Processors.INTEL;
 
+//        Outputs
         System.out.println("Russia ATM with number "+atmRussia+" has "+comp1+" processor");
         System.out.println("Hong Kong ATM with number "+atmHongKong+" has "+comp2+" processor");
     }
