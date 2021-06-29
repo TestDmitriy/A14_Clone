cec64013174f874fbfbb4245993b6c1434b9ce86
baur <baurzhan.zh@gmail.com>
2021-06-28 21:35:34 -0400

    Lesson 7 - Enums

---
+++ src/main/java/lesson7/App.java
@@ -0,0 +1,21 @@
+package lesson7;
+
+public class App {
+    public static void main(String[] args) {
+        Colors car = Colors.RED;
+        car = Colors.BLUE;
+        car = Colors.YELLOW;
+
+        String car1 = "Red";
+        String car2 = "RED";
+        String car3 = "red";
+
+        boolean x = true; // false;
+
+        SchoolType mySchool = SchoolType.PUBLIC;
+
+        SchoolAge theSchool = SchoolAge.MIDDLE;
+
+        Size[] snickers = {Size.XL,Size.L,Size.M};
+    }
+}
