d102481cb7e014d437a61e9a5b99918f1308906a
TestDmitriy <qakargapolov@gmail.com>
2021-06-29 20:14:29 -0400

    Lesson 07 / HomeWork / Github connected

---
+++ src/main/java/hw07/enumsd.java
@@ -0,0 +1,10 @@
+package hw07;
+
+    public enum enumsd                  //  Should be NAMED the same as - class (TAB)
+    {
+            I3,
+            I5,
+            I7,
+            I9,
+    }
+
