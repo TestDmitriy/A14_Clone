5fc16ff0fcee6313dcd395d5702d4fa78b07d866
baur <baurzhan.zh@gmail.com>
2021-07-01 21:17:28 -0400

    Lesson 10 - Inheritance

---
+++ src/main/java/lesson10/Student.java
@@ -0,0 +1,26 @@
+package lesson10;
+
+import lesson9a.Subjects;
+
+public class Student extends Person{
+    private Subjects major;
+
+    public Student(String name, String lastName, int year, Subjects major) {
+        super(name,lastName,year);
+        this.major = major;
+    }
+
+    public Subjects getMajor() {
+        return major;
+    }
+
+    public void setMajor(Subjects major) {
+        this.major = major;
+    }
+
+    @Override
+    public String toString() {
+        return "name=" + name + "\nlastName=" + lastName + "\nyear=" + year +
+                "\nmajor=" + major;
+    }
+}
