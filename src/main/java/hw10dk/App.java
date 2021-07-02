1c2a8403a444f68dcf8faf8fbd16784a5adad9e4
TestDmitriy <qakargapolov@gmail.com>
2021-07-02 01:11:50 -0400

    // HOMEWORK

---
+++ src/main/java/hw10dk/App.java
@@ -0,0 +1,17 @@
+package hw10dk;
+
+public class App {
+    public static void main(String[] args) {
+// Lesson 10:
+//          1. Create a super class and 3 child classes
+//             create 2 object of each class (including superclass - don't make it abstract)
+//--------------------------------------------
+
+
+
+
+
+
+
+    }
+}
