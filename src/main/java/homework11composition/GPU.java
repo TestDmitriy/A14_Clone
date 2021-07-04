5793799dfefa63130ae933ea7c0f1dd020002272
Alex <avoronovski@gmail.com>
2021-07-03 21:12:03 -0500

    Lesson 11 - Composition. Finish Computer. Create 2 instances of computer. get CPU model from computer

--- src/main/java/homework11composition/GPU.java
+++ src/main/java/homework11composition/GPU.java
@@ -1,27 +1,16 @@
 package homework11composition;
 
 public class GPU extends Device {
-    private int tFlops;
     private boolean isRTX;
 
-    public GPU(String brand, String model, Colors color, int tFlops, boolean isRTX) {
+    public GPU(String brand, String model, Colors color, boolean isRTX) {
         super(brand, model, color);
-        this.tFlops = tFlops;
         this.isRTX = isRTX;
     }
 
-    public GPU(String brand, String model, int tFlops, boolean isRTX) {
+    public GPU(String brand, String model, boolean isRTX) {
         super(brand, model);
-        this.tFlops = tFlops;
         this.isRTX = isRTX;
-    }
-
-    public int gettFlops() {
-        return tFlops;
-    }
-
-    public void settFlops(int tFlops) {
-        this.tFlops = tFlops;
     }
 
     public boolean isRTX() {
--- src/main/java/homework11composition/SystemBlock.java
+++ src/main/java/homework11composition/SystemBlock.java
@@ -6,4 +6,52 @@ public class SystemBlock {
     private GPU gpu;
     private Disk disk;
     private MotherBoard mb;
+
+    public SystemBlock(PowerSupply powerSupply, Box box, GPU gpu, Disk disk, MotherBoard mb) {
+        this.powerSupply = powerSupply;
+        this.box = box;
+        this.gpu = gpu;
+        this.disk = disk;
+        this.mb = mb;
+    }
+
+    public PowerSupply getPowerSupply() {
+        return powerSupply;
+    }
+
+    public void setPowerSupply(PowerSupply powerSupply) {
+        this.powerSupply = powerSupply;
+    }
+
+    public Box getBox() {
+        return box;
+    }
+
+    public void setBox(Box box) {
+        this.box = box;
+    }
+
+    public GPU getGpu() {
+        return gpu;
+    }
+
+    public void setGpu(GPU gpu) {
+        this.gpu = gpu;
+    }
+
+    public Disk getDisk() {
+        return disk;
+    }
+
+    public void setDisk(Disk disk) {
+        this.disk = disk;
+    }
+
+    public MotherBoard getMb() {
+        return mb;
+    }
+
+    public void setMb(MotherBoard mb) {
+        this.mb = mb;
+    }
 }
