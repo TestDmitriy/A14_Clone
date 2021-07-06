9c1743ee16cb3e824c2fd46a24f74d55f14297f5
Alex <avoronovski@gmail.com>
2021-07-06 17:44:27 -0500

    Lesson 12 - Interfaces, Polymorphism

---
+++ src/main/java/homework12intpoly/App.java
@@ -0,0 +1,11 @@
+package homework12intpoly;
+
+//  Create an interface and 2 classes which implements it
+//  Create 2 objects of each class
+//  Assign one object to interface type
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
